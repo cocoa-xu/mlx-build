61dcf7cbac2b5092f6c8ba5f577c84dd3bf07521
Cocoa <i@uwucocoa.moe>
2025-02-13 13:15:20 +0100

    fix build script
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- .github/do-build.sh
+++ .github/do-build.sh
@@ -14,8 +14,9 @@ mkdir -p "${DESTDIR}"
 
 export DEBIAN_FRONTEND=noninteractive
 
-apt-get update
-apt-get install -y gcc g++ curl make cmake automake autoconf pkg-config git
+export SUDO="$(which sudo)"
+${SUDO} apt-get update
+${SUDO} apt-get install -y gcc g++ curl make cmake automake autoconf pkg-config git
 
 cd "${ROOTDIR}"
 if [ ! -f "${MLX_SRC_FILENAME}" ]; then
